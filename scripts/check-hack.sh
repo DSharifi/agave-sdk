b66e5f719ac9a57be1b9909b52272bf5ee2013ce
yihau <yihau.chen@icloud.com>
2026-07-09 00:49:39 +0800

    ci: remove --ignore-unknown-features from scripts/check-hack.sh

--- scripts/check-hack.sh
+++ scripts/check-hack.sh
@@ -5,4 +5,4 @@ here="$(dirname "$0")"
 src_root="$(readlink -f "${here}/..")"
 cd "${src_root}"
 
-./cargo nightly hack check --all-targets --locked --ignore-unknown-features
+./cargo nightly hack check --all-targets --locked
