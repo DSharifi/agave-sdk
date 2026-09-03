c65908a848231c342ff04eb8c3bdbc032b19017c
Daniel Sharifi <me@dsharifi.com>
2026-09-03 22:27:07 +0200

    test: add tests to verify event trait is implemented

--- event-system/tests/event.rs
+++ event-system/tests/event.rs
@@ -1,4 +1,13 @@
-use agave_event_system::{Event, event};
+use {
+    agave_event_system::{Event, event},
+    rstest::rstest,
+    std::mem::size_of,
+};
+
+#[event]
+enum StaticEnumEvent {
+    Event1 { value: u64 },
+}
 
 #[event]
 struct StaticEvent {
@@ -10,18 +19,48 @@ struct DynamicEvent {
     values: Vec<u8>,
 }
 
+#[event(max_serialized_size = 64)]
+enum DynamicEnumEvent {
+    Event1 { values: Vec<u8> },
+}
+
 #[test]
-fn event_macro_sizes_static_queue_cells_from_the_schema() {
+fn event_macro_sizes_static_struct_queue_cell_from_the_schema() {
     assert_eq!(
-        std::mem::size_of::<<StaticEvent as Event>::QueueCell>(),
-        std::mem::size_of::<u64>()
+        size_of::<<StaticEvent as Event>::QueueCell>(),
+        size_of::<u64>()
     );
 }
 
 #[test]
-fn event_macro_uses_the_declared_dynamic_size_bound() {
+fn event_macro_sizes_static_enum_queue_cell_from_the_schema() {
+    let tag_size = size_of::<u32>();
+    let payload_size = size_of::<u64>();
+    let expected_size = tag_size.checked_add(payload_size).unwrap();
+
     assert_eq!(
-        std::mem::size_of::<<DynamicEvent as Event>::QueueCell>(),
-        64
+        size_of::<<StaticEnumEvent as Event>::QueueCell>(),
+        expected_size
     );
+}
+
+#[test]
+fn event_macro_uses_the_declared_dynamic_size_bound_for_struct() {
+    assert_eq!(size_of::<<DynamicEvent as Event>::QueueCell>(), 64);
+}
+
+#[test]
+fn event_macro_uses_the_declared_dynamic_size_bound_for_enum() {
+    assert_eq!(size_of::<<DynamicEnumEvent as Event>::QueueCell>(), 64);
+}
+
+#[rstest]
+#[case::static_struct(StaticEvent { value: 1 })]
+#[case::static_enum(StaticEnumEvent::Event1 { value: 1 })]
+#[case::dynamic_struct(DynamicEvent { values: vec![1, 2, 3] })]
+#[case::dynamic_enum(DynamicEnumEvent::Event1 { values: vec![1, 2, 3] })]
+fn event_macro_implements_event_trait(#[case] event: impl Event) {
+    // drop so `event` is not dead code.
+    // clippy give false positive about it when it's underscored.
+    drop(event)
 }
